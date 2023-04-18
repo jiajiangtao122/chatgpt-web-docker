a632a19a669f27bfe7ae7b77162b6eff47b0e5c6
jiangtaoj <48207940+jiajiangtao122@users.noreply.github.com>
2023-04-19 00:33:04 +0800

    v1.3

--- chatgpt-web/admin/assets/consumer.48ac3450.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index.dc7cd72b.js";function u(e){return r.get({url:"/user/list",params:e})}function s(e){return r.get({url:"/user/detail",params:e})}function n(e){return r.post({url:"/user/edit",params:e})}function i(e){return r.get({url:"/user/delete",params:e})}export{u as a,s as g,i as r,n as u};
--- chatgpt-web/admin/assets/website.52bc9b6f.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index.dc7cd72b.js";function r(){return e.get({url:"/setting/base/detail"})}function i(t){return e.post({url:"/setting/base/save",params:t})}export{r as g,i as s};
